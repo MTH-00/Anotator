dd84a89a7c7aa3d7c28bc9f9bbe4e58e06a03684
Shameerisb <142782978+Shameerisb@users.noreply.github.com>
2023-12-22 04:25:09 +0500

    Delete Source_code directory

--- Source_code/main.py
+++
@@ -1,8 +0,0 @@
-import tkinter as tk
-from ImageAnnotationTool import ImageAnnotationTool
-
-
-if __name__ == "__main__":
-    root = tk.Tk()
-    app = ImageAnnotationTool(root)
-    root.mainloop()
